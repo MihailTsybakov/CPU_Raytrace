be31299c5ff4e0a91032b3ca2a6ffb79b74fa853
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2021-04-11 21:25:57 +0300

    Added Video Join Example

--- main.cpp
+++ main.cpp
@@ -9,5 +9,14 @@ int main(int argc, char* argv[])
     raytracer rtracer("instructions.txt", "objects.txt", "trace_vs.bmp", 4);
     rtracer.raytrace();
     //check_system_cores();
+    
+    /*
+    R.join_to_video("frame_%d.bmp",
+        "video.avi", 
+        24,      // framerate
+        1,       // quality 1 = best, 30 = worst 
+        "ffmpeg" /*ffmpeg path*/);
+    */
+    
     return 0;
 }
