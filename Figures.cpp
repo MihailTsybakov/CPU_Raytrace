5d38a9b70ab6fb7016152959cbef8e08f0f865f1
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2021-05-13 09:38:52 +0300

    Ochepyatka Fixed

--- Figures.cpp
+++ Figures.cpp
@@ -51,7 +51,7 @@ box::box(double x1, double y1, double z1,
     v2 = { x1 + dx, y1, z1 };
     v3 = { x1, y1 + dy, z1 };
     v4 = { x1 + dx, y1 + dy, z1 };
-    v5 = { x1, y1, z1 + dx };
+    v5 = { x1, y1, z1 + dz };
     v6 = { x1 + dx, y1, z1 + dz };
     v7 = { x1, y1 + dy, z1 + dz };
 
@@ -165,8 +165,9 @@ bool sphere::traceback(const vector<double>& s_point,
     return true;
 }
 
-bool box::internal(vector<double> point, int edge_number) const
+bool box::internal_point(vector<double> point, int edge_number) const
 {
+    // edge_1 = 1, 2, 4, 3; edge_2 = 1, 5, 7, 3; edge_3 = 1, 5, 6, 2; edge_4 = 3, 7, 8, 4; edge_5 = 2, 6, 8, 4; edge_6 = 5, 6, 8, 7;
     try
     {
         switch (edge_number)
@@ -242,131 +243,124 @@ bool box::traceback(const vector<double>& s_point,
     vector<double>& res_v,
     int trace_mode)
 {
-    vector<vector<double>> intersections;
-    vector<vector<double>> Ns;
-    vector<double> N;
-    vector<double> center = { v8[0] - v1[0] / 2, v8[1] - v1[1] / 2, v8[2] - v1[2] / 2 };
-    vector<double> center_vect = center - s_point;
+    std::vector<std::vector<double>> intersections;
+    std::vector<std::vector<double>> normals;
+    double alpha = trace_ray[0], beta = trace_ray[1], gamma = trace_ray[2];
+    bool inner = false;
     // edge_1 = 1, 2, 4, 3; edge_2 = 1, 5, 7, 3; edge_3 = 1, 5, 6, 2; edge_4 = 3, 7, 8, 4; edge_5 = 2, 6, 8, 4; edge_6 = 5, 6, 8, 7;
-    // Distance between box center and trace ray:
-    double dist = scalmul(center_vect, center_vect) - pow(scalmul(center_vect, trace_ray), 2) / scalmul(trace_ray, trace_ray);
-    if (dist > half_diag_quad)
+
+    // Edge 1:
+    if (gamma != 0)
     {
+        double parameter = (v1[2] - s_point[2]) / gamma;
+        if (parameter == 0) inner = true;
+        double x = s_point[0] + alpha * parameter;
+        double y = s_point[1] + beta * parameter;
+        std::vector<double> point = { x,y,v1[2] };
+        if (internal_point(point, 1) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({ 0,0,-1 });
+        }
+    }
+    // Edge 2:
+    if (alpha != 0)
+    {
+        double parameter = (v1[0] - s_point[0]) / alpha;
+        if (parameter == 0) inner = true;
+        double y = s_point[1] + beta * parameter;
+        double z = s_point[2] + gamma * parameter;
+        std::vector<double> point = { v1[0], y, z };
+        if (internal_point(point, 2) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({ -1, 0, 0 });
+        }
+    }
+    // Edge 3:
+    if (beta != 0)
+    {
+        double parameter = (v1[1] - s_point[1]) / beta;
+        if (parameter == 0) inner = true;
+        double x = s_point[0] + alpha * parameter;
+        double z = s_point[2] + gamma * parameter;
+        std::vector<double> point = { x, v1[1], z };
+        if (internal_point(point, 3) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({ 0, -1, 0 });
+        }
+    }
+    // Edge 4:
+    if (beta != 0)
+    {
+        double parameter = (v3[1] - s_point[1]) / beta;
+        if (parameter == 0) inner = true;
+        double x = s_point[0] + alpha * parameter;
+        double z = s_point[2] + gamma * parameter;
+        std::vector<double> point = { x, v3[1], z };
+        if (internal_point(point, 4) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({ 0,1,0 });
+        }
+    }
+    // Edge 5:
+    if (alpha != 0)
+    {
+        double parameter = (v2[0] - s_point[0]) / alpha;
+        if (parameter == 0) inner = true;
+        double y = s_point[1] + beta * parameter;
+        double z = s_point[2] + gamma * parameter;
+        std::vector<double> point = { v2[0], y, z };
+        if (internal_point(point, 5) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({ 1,0,0 });
+        }
+    }
+    // Edge 6:
+    if (gamma != 0)
+    {
+        double parameter = (v8[2] - s_point[2]) / gamma;
+        if (parameter == 0) inner = true;
+        double x = s_point[0] + alpha * parameter;
+        double y = s_point[1] + beta * parameter;
+        std::vector<double> point = { x, y, v8[2] };
+        if (internal_point(point, 6) == true)
+        {
+            intersections.push_back(point);
+            normals.push_back({0,0,1});
+        }
+    }
+    if (trace_mode == 2)
+    {
+        if (intersections.size() > 0 && inner == false) return true;
         return false;
     }
-    // Checking intersection with edge 1:
-    if (trace_ray[2] != 0)
-    {
-        double param = (v1[2] - s_point[2]) / trace_ray[2];
-        double x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param;
-        vector<double> point = { x, y, v1[2] };
-        if (internal(point, 1) == true)
-        {
-            intersections.push_back(point);
-            N = { 0, 0, -1 };
-            Ns.push_back(N);
-        }
-    }
-    // edge 2:
-    if (trace_ray[0] != 0)
-    {
-        double param = (v1[0] - s_point[0]) / trace_ray[0];
-        double y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
-        vector<double> point = { v1[0], y, z };
-        if (internal(point, 2) == true)
-        {
-            intersections.push_back(point);
-            N = { -1, 0, 0 };
-            Ns.push_back(N);
-        }
-    }
-    // edge 3:
-    if (trace_ray[1] != 0)
-    {
-        double param = (v1[1] - s_point[1]) / trace_ray[1];
-        double x = s_point[0] + trace_ray[0] * param, z = s_point[2] + trace_ray[2] * param;
-        vector<double> point = { x, v1[1], z };
-        if (internal(point, 3) == true)
-        {
-            intersections.push_back(point);
-            N = { 0, -1, 0 };
-            Ns.push_back(N);
-        }
-    }
-    // edge 4:
-    if (trace_ray[1] != 0)
-    {
-        double param = (v8[1] - s_point[1]) / trace_ray[1];
-        double x = s_point[0] + trace_ray[0] * param, z = s_point[2] + trace_ray[2] * param;
-        vector<double> point = { x, v8[1], z };
-        if (internal(point, 4) == true)
-        {
-            intersections.push_back(point);
-            N = { 0, 1, 0 };
-            Ns.push_back(N);
-        }
-
-    }
-    // edge 5:
-    if (trace_ray[0] != 0)
-    {
-        double param = (v8[0] - s_point[0]) / trace_ray[0];
-        double y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
-        vector<double> point = { v8[0], y, z };
-        if (internal(point, 5) == true)
-        {
-            intersections.push_back(point);
-            N = { 1, 0, 0 };
-            Ns.push_back(N);
-        }
-
-    }
-    // edge 6:
-    if (trace_ray[2] != 0)
-    {
-        double param = (v8[2] - s_point[2]) / trace_ray[2];
-        double x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param;
-        vector<double> point = { x, y, v8[2] };
-        if (internal(point, 6) == true)
-        {
-            intersections.push_back(point);
-            N = { 0, 0, 1 };
-            Ns.push_back(N);
-        }
-
-    }
-    if (intersections.size() == 0 || intersections.size() == 3)
-    {
-        return false;
-    }
-    vector<double> ans;
+    // res_v = {x, y, z, R, G, B, Nx, Ny, Nz, Surface, Otype}
     if (intersections.size() == 2)
     {
-        double distance_1 = dist_quad(s_point, intersections[0]), distance_2 = dist_quad(s_point, intersections[1]);
-        if (distance_1 <= distance_2)
+        double D1 = dist_quad(intersections[0], s_point);
+        double D2 = dist_quad(intersections[1], s_point);
+        res_v[3] = R; res_v[4] = G; res_v[5] = B;
+        res_v[9] = surface; res_v[10] = 2;
+        if (D1 <= D2)
         {
-            ans = intersections[0];
-            N = Ns[0];
+            res_v[0] = intersections[0][0]; res_v[1] = intersections[0][1]; res_v[2] = intersections[0][2];
+            res_v[6] = normals[0][0]; res_v[7] = normals[0][1]; res_v[8] = normals[0][2];
         }
         else
         {
-            ans = intersections[1];
-            N = Ns[1];
+            res_v[0] = intersections[1][0]; res_v[1] = intersections[1][1]; res_v[2] = intersections[1][2];
+            res_v[6] = normals[1][0]; res_v[7] = normals[1][1]; res_v[8] = normals[1][2];
         }
+        return true;
     }
-    if (intersections.size() == 1)
-    {
-        ans = intersections[0];
-        N = Ns[0];
-    }
-    // result = {x, y, z, R, G, B, Nx, Ny, Nz, Surface}
-    res_v[0] = ans[0]; res_v[1] = ans[1]; res_v[2] = ans[2];
-    res_v[3] = R; res_v[4] = G; res_v[5] = B; res_v[9] = surface;
-    res_v[6] = N[0], res_v[7] = N[1], res_v[8] = N[2]; res_v[10] = 2;
-    return true;
+    return false;
 }
 
-bool tetra::internal(vector<double> point, vector<vector<double>> edge) const
+bool tetra::internal_point(vector<double> point, vector<vector<double>> edge) const
 {
     vector<double> sign;
     sign.push_back(vectmul_area(edge[1] - edge[0], point - edge[0]));
@@ -413,7 +407,7 @@ bool tetra::traceback(const vector<double>& s_point,
             N[2] * s_point[2]) / (N[0] * trace_ray[0] + N[1] * trace_ray[1] + N[2] * trace_ray[2]);
         x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
         point = { x,y,z };
-        if (internal(point, edge_1) == true)
+        if (internal_point(point, edge_1) == true)
         {
             intersections.push_back(point);
             Ns.push_back(N);
@@ -430,7 +424,7 @@ bool tetra::traceback(const vector<double>& s_point,
             N[2] * s_point[2]) / (N[0] * trace_ray[0] + N[1] * trace_ray[1] + N[2] * trace_ray[2]);
         x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
         point = { x,y,z };
-        if (internal(point, edge_2) == true)
+        if (internal_point(point, edge_2) == true)
         {
             intersections.push_back(point);
             Ns.push_back(N);
@@ -447,7 +441,7 @@ bool tetra::traceback(const vector<double>& s_point,
             N[2] * s_point[2]) / (N[0] * trace_ray[0] + N[1] * trace_ray[1] + N[2] * trace_ray[2]);
         x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
         point = { x,y,z };
-        if (internal(point, edge_3) == true)
+        if (internal_point(point, edge_3) == true)
         {
             intersections.push_back(point);
             Ns.push_back(N);
@@ -464,7 +458,7 @@ bool tetra::traceback(const vector<double>& s_point,
             N[2] * s_point[2]) / (N[0] * trace_ray[0] + N[1] * trace_ray[1] + N[2] * trace_ray[2]);
         x = s_point[0] + trace_ray[0] * param, y = s_point[1] + trace_ray[1] * param, z = s_point[2] + trace_ray[2] * param;
         point = { x,y,z };
-        if (internal(point, edge_4) == true)
+        if (internal_point(point, edge_4) == true)
         {
             intersections.push_back(point);
             Ns.push_back(N);
